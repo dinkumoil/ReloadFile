20a483ef6723feb38124cd07d2705eca34ddd8d3
Andreas Heim <dinkumoil@users.noreply.github.com>
2019-09-17 11:38:45 +0200

    Released v1.0

---
+++ src/Main.pas
@@ -0,0 +1,190 @@
+{
+    This file is part of the ReloadFile plugin for Notepad++
+    Author: Andreas Heim
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License version 3 as published
+    by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+}
+
+unit Main;
+
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.StrUtils, System.DateUtils,
+  System.IOUtils, System.Math, System.Types, System.Classes, System.Generics.Defaults,
+  System.Generics.Collections, Vcl.Graphics,
+
+  SciSupport, NppSupport, NppMenuCmdID, NppPlugin, NppPluginForms, NppPluginDockingForms,
+
+  dialog_TfrmAbout;
+
+
+type
+  // Plugin class
+  TReloadFilePlugin = class(TNppPlugin)
+  private
+    FMenuItemIdxReloadFile: integer;
+    FMenuItemIdxAbout:      integer;
+    FToolbarBmp:            TBitmap;
+
+  protected
+    // Handler for certain Notepad++ events
+    procedure   DoNppnToolbarModification; override;
+
+  public
+    constructor Create; override;
+    destructor  Destroy; override;
+
+    // Function to reload the file of the currently active tab
+    procedure   DoReloadFile;
+    
+  end;
+
+
+var
+  // Class type to create in startup code
+  PluginClass: TNppPluginClass = TReloadFilePlugin;
+
+  // Plugin instance variable, this is the reference to use in plugin's code
+  Plugin: TReloadFilePlugin;
+
+
+
+implementation
+
+{$R .\Res\images.res}
+
+
+const
+  // Plugin name
+  TXT_PLUGIN_NAME:         string = 'ReloadFile';
+
+  TXT_MENUITEM_RELOADFILE: string = 'Reload File';
+  TXT_MENUITEM_ABOUT:      string = 'About';
+
+  ID_TOOLBAR_ICON:         string = 'TOOLBAR_ICON';
+
+
+// Functions associated to the plugin's Notepad++ menu entries
+procedure ReloadFile; cdecl; forward;
+procedure ShowAbout; cdecl; forward;
+
+
+// =============================================================================
+// Class TReloadFilePlugin
+// =============================================================================
+
+// -----------------------------------------------------------------------------
+// Create / Destroy
+// -----------------------------------------------------------------------------
+
+constructor TReloadFilePlugin.Create;
+begin
+  inherited Create;
+
+  // Store a reference to the instance in a global variable with an appropriate
+  // type to get access to its properties and methods
+  Plugin := Self;
+
+  // This property is important to extract version infos from the DLL file,
+  // so set it right now after creation of the object
+  PluginName := TXT_PLUGIN_NAME;
+
+  // Add plugins's menu entries to Notepad++
+  FMenuItemIdxReloadFile := AddFuncItem(TXT_MENUITEM_RELOADFILE, Main.ReloadFile);
+  FMenuItemIdxAbout      := AddFuncItem(TXT_MENUITEM_ABOUT,      Main.ShowAbout);
+end;
+
+
+destructor TReloadFilePlugin.Destroy;
+begin
+  // Cleanup
+  FToolbarBmp.Free;
+
+  // It's totally legal to call Free on already freed instances,
+  // no checks needed
+  frmAbout.Free;
+
+  inherited;
+end;
+
+
+// -----------------------------------------------------------------------------
+// (De-)Initialization
+// -----------------------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------------------
+// Event handler
+// -----------------------------------------------------------------------------
+
+// Called when Notepad++ is ready for toolbar modifications
+procedure TReloadFilePlugin.DoNppnToolbarModification;
+var
+  IconData: TToolbarIcons;
+
+begin
+  inherited;
+
+  FToolbarBmp := TBitmap.Create;
+
+  FToolbarBmp.LoadFromResourceName(HInstance, ID_TOOLBAR_ICON);
+  FToolbarBmp.PixelFormat := pf8Bit;
+
+  IconData.ToolbarBmp  := FToolbarBmp.Handle;
+  IconData.ToolbarIcon := 0;
+
+  AddToolbarIcon(CmdIdFromMenuItemIdx(FMenuItemIdxReloadFile), IconData);
+end;
+
+
+// -----------------------------------------------------------------------------
+// Worker methods
+// -----------------------------------------------------------------------------
+
+// Request file reload
+procedure TReloadFilePlugin.DoReloadFile;
+begin
+  ReloadCurrentFile(false);
+end;
+
+
+
+// -----------------------------------------------------------------------------
+// Plugin menu items
+// -----------------------------------------------------------------------------
+
+// Reload file of currently active tab
+procedure ReloadFile; cdecl;
+begin
+  Plugin.DoReloadFile();
+end;
+
+
+// Show "About" dialog in Notepad++
+procedure ShowAbout; cdecl;
+begin
+  if not Assigned(frmAbout) then
+  begin
+    // Show about dialog in a modal state and destroy it after close
+    frmAbout := TfrmAbout.Create(Plugin);
+    frmAbout.ShowModal;
+    frmAbout.Free;
+  end;
+end;
+
+
+end.
